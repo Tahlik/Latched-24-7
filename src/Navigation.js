df20b5772b1ad319c2501cdca03492cb22e99856
Tahlik <brich003@fiu.edu>
2019-10-28 21:09:13 -0400

    Update Navigation.js

--- src/Navigation.js
+++ src/Navigation.js
@@ -8,46 +8,133 @@ import {
   Typography
 } from "@material-ui/core";
 import RefreshIcon from "@material-ui/icons/Refresh";
+import ButtonBase from '@material-ui/core/ButtonBase';
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
-  menuButton: {
-    marginRight: 16
+const images = [
+  {
+    url: 'https://media.npr.org/assets/img/2017/06/23/tri-breastfeeding_custom-7dba21ccb308a4556d7caf9176765e9f97cc6028-s800-c85.jpg',
+    title: 'Breastfeeding Mothers',
+    width: '100%',
+    height: 100,
   },
-  title: {
-    flexGrow: 1
-  }
-});
+  {
+    url: 'https://www.nursingexplorer.com/img/articles/careers/42343242.jpg',
+    title: 'Lactation Professionals',
+    width: '100%',
+  },
+];
 
-const Navigation = ({ reloadShelters }) => {
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    minWidth: 300,
+    width: '100%',
+  },
+  image: {
+    position: 'relative',
+    height: 400,
+    [theme.breakpoints.down('xs')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 100,
+    },
+    '&:hover, &$focusVisible': {
+      zIndex: 1,
+      '& $imageBackdrop': {
+        opacity: 0.15,
+      },
+      '& $imageMarked': {
+        opacity: 0,
+      },
+      '& $imageTitle': {
+        border: '4px solid currentColor',
+      },
+    },
+  },
+  focusVisible: {},
+  imageButton: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
+  },
+  imageSrc: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%',
+  },
+  imageBackdrop: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity'),
+  },
+  imageTitle: {
+    position: 'relative',
+    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity'),
+  },
+}));
+
+export default function ButtonBases() {
   const classes = useStyles();
-  const isLoggedIn = true;
 
   return (
-    <AppBar position="sticky" elevation={0} square={false}>
-      <Toolbar>
-        <Typography variant="h6" className={classes.title}>
-          Latched 24/7
-        </Typography>
-        <IconButton
-          edge="end"
-          className={classes.menuButton}
-          color="inherit"
-          onClick={() => reloadShelters()}
+    <div className={classes.root}>
+      {images.map(image => (
+        <ButtonBase
+          focusRipple
+          key={image.title}
+          className={classes.image}
+          focusVisibleClassName={classes.focusVisible}
+          style={{
+            width: image.width,
+          }}
         >
-          <RefreshIcon />
-        </IconButton>
-        {isLoggedIn && (
-          <Switch
-            checked={true}
-            onChange={() => {}}
-            value="checkedA"
-            inputProps={{ 'aria-label': 'secondary checkbox' }}
+          <span
+            className={classes.imageSrc}
+            style={{
+              backgroundImage: `url(${image.url})`,
+            }}
           />
-        )}
-      </Toolbar>
-    </AppBar>
+          <span className={classes.imageBackdrop} />
+          <span className={classes.imageButton}>
+            <Typography
+              component="span"
+              variant="subtitle1"
+              color="inherit"
+              className={classes.imageTitle}
+            >
+              {image.title}
+              <span className={classes.imageMarked} />
+            </Typography>
+          </span>
+        </ButtonBase>
+      ))}
+    </div>
   );
-};
+}
 
-export default Navigation;
