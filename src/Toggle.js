224084d7163c45658de4e3becc64bf8abeb5f08d
dorriola <56094680+dorriola@users.noreply.github.com>
2019-10-30 11:06:29 -0400

    Update Toggle.js

--- src/Toggle.js
+++ src/Toggle.js
@@ -17,13 +17,60 @@ const useStyles = makeStyles({
   }
 });
 
-const Toggle = () => {
-  const classes = useStyles();
-  const [isOnlineToggle, setIsOnlineToggle] = useState(true);
-  const [_, setStatus] = useAxios(
-    `/.netlify/functions/setStatus?is_online=${isOnlineToggle}`,
-    { manual: true }
-  );
+const IOSSwitch = withStyles(theme => ({
+  root: {
+    width: 42,
+    height: 26,
+    padding: 0,
+    margin: theme.spacing(1),
+  },
+  switchBase: {
+    padding: 1,
+    '&$checked': {
+      transform: 'translateX(16px)',
+      color: theme.palette.common.white,
+      '& + $track': {
+        backgroundColor: '#52d869',
+        opacity: 1,
+        border: 'none',
+      },
+    },
+    '&$focusVisible $thumb': {
+      color: '#52d869',
+      border: '6px solid #fff',
+    },
+  },
+  thumb: {
+    width: 24,
+    height: 24,
+  },
+  const IOSSwitch = withStyles(theme => ({
+    root: {
+      width: 42,
+      height: 26,
+      padding: 0,
+      margin: theme.spacing(1),
+    },
+    switchBase: {
+      padding: 1,
+      '&$checked': {
+        transform: 'translateX(16px)',
+        color: theme.palette.common.white,
+        '& + $track': {
+          backgroundColor: '#52d869',
+          opacity: 1,
+          border: 'none',
+        },
+      },
+      '&$focusVisible $thumb': {
+        color: '#52d869',
+        border: '6px solid #fff',
+      },
+    },
+    thumb: {
+      width: 24,
+      height: 24,
+    },
 
   const setOnline = status => {
     setIsOnlineToggle(status);
